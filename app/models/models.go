f905d8fe3b942e630be576669416b6859ce44363
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-15 23:16:14 +0430

    Added models

--- app/models/models.go
+++ app/models/models.go
@@ -1,2 +1,39 @@
 package models
 
+import (
+	"encoding/json"
+	"io"
+)
+
+type Model struct {
+}
+
+// ToJSON serializes the contents of the collection to JSON
+// NewEncoder provides better performance than json.Unmarshal as it does not
+// have to buffer the output into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+//
+// https://golang.org/pkg/encoding/json/#NewEncoder
+func (m *Model) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(m)
+}
+
+//FromJSON is like ToJSON but in reverse way
+func (m *Model) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(m)
+}
+
+type User struct {
+	Model
+	Mobile string  `json:"mobile"`
+	Credit float64 `json:"credit"`
+}
+
+type Code struct {
+	Model
+	Code          string  `json:"code"`
+	Credit        float64 `json:"credit"`
+	ConsumerCount float64 `json:"consumerCount"`
+}
