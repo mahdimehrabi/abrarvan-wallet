f5f7b0bb0d501fe6c46f21a845dd155ae0198473
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-16 16:04:06 +0430

    Fixed migration syntax problem

--- migrations/000001_user.up.sql
+++ migrations/000001_user.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "users" (
     mobile      VARCHAR(15) UNIQUE NOT NULL PRIMARY KEY,
-    credit      DECIMAL DEFAULT 0
+    credit      DECIMAL DEFAULT 0,
     recevied_charge BOOLEAN DEFAULT FALSE
-)
+);
 
-CREATE TABLE "code"(
+CREATE TABLE "codes"(
     code VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY,
     credit  DECIMAL DEFAULT 1000000,
     consumer_count  int default 1000
-)
+);
 
