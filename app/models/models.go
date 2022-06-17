2d98f3a97432244f9d84cd13bc4856ee7493bfb6
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-17 11:18:42 +0430

    Delete unused codes

--- app/models/models.go
+++ app/models/models.go
@@ -5,36 +5,25 @@ import (
 	"io"
 )
 
-type Model struct {
-}
-
-// ToJSON serializes the contents of the collection to JSON
-// NewEncoder provides better performance than json.Unmarshal as it does not
-// have to buffer the output into an in memory slice of bytes
-// this reduces allocations and the overheads of the service
-//
-// https://golang.org/pkg/encoding/json/#NewEncoder
-func (m *Model) ToJSON(w io.Writer) error {
-	e := json.NewEncoder(w)
-	return e.Encode(m)
-}
-
-//FromJSON is like ToJSON but in reverse way
-func (m *Model) FromJSON(r io.Reader) error {
-	e := json.NewDecoder(r)
-	return e.Decode(m)
-}
-
 type User struct {
-	Model
 	Mobile         string  `json:"mobile"`
 	Credit         float64 `json:"credit"`
 	ReceivedCharge bool    `json:"receivedCharge"`
 }
 
 type Code struct {
-	Model
 	Code          string  `json:"code"`
 	Credit        float64 `json:"credit"`
 	ConsumerCount int     `json:"consumerCount"`
+}
+
+func (m *Code) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(m)
+}
+
+//FromJSON is like ToJSON but in reverse way
+func (m *Code) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(m)
 }
