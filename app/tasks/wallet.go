96aac27a2af345ccf081d197fa53b718ccdde0f6
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-18 15:33:38 +0430

    Added periodic wallet codes update

--- app/tasks/wallet.go
+++ app/tasks/wallet.go
@@ -3,8 +3,10 @@ package tasks
 import (
 	"challange/app/infrastracture"
 	"challange/app/interfaces"
+	"challange/app/models"
 	"context"
 	"github.com/hibiken/asynq"
+	"strings"
 	"time"
 )
 
@@ -39,5 +41,29 @@ func (et *WalletTask) NewUpdateCodesWalletTask() (*asynq.Task, error) {
 
 //this method get count of Wallets users and Store count of Wallet users in memory db
 func (et WalletTask) HandleWalletCodesUpdateTask(ctx context.Context, t *asynq.Task) error {
+	rows, err := et.db.Query(ctx, "SELECT * FROM codes WHERE consumer_count>0", []interface{}{})
+	if err != nil {
+		return err
+	}
+	for _, row := range rows {
+		code := row[0].(string)
+		codeJosn, err := et.memoryDB.Get("code_" + code)
+		if err != nil {
+			et.logger.Error(err.Error())
+			continue
+		}
+		codeModel := models.Code{}
+		err = codeModel.FromJSON(strings.NewReader(codeJosn))
+		if err != nil {
+			et.logger.Error(err.Error())
+			continue
+		}
+		_, err = et.db.Exec(ctx,
+			"UPDATE codes SET consumer_count=$1 WHERE code=$2",
+			[]interface{}{codeModel.ConsumerCount, code})
+		if err != nil {
+			et.logger.Error(err.Error())
+		}
+	}
 	return nil
 }
